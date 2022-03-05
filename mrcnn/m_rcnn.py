ce8858f17a3a0cdec837f337e89fe53497fa9c5b
Nisarg00 <59718682+Nisarg00@users.noreply.github.com>
2022-03-05 10:57:36 +0530

    Update m_rcnn.py

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -14,7 +14,7 @@ import shutil
 import zipfile
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("/content/Hack_master")
+ROOT_DIR = os.path.abspath("/content/Hack_master/mrcnn")
 print("VERS 0.2")
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
